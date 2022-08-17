31f8d43a08d35140dd475cc58a6dbf9a7b6cffb8
Mark Thompson <mthompson@bitovi.com>
2022-08-17 10:05:05 -0700

    first commit

---
+++ src/movie/movie.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { Movie } from '@prisma/client';
+import { PrismaService } from 'server/prisma/prisma.service';
+
+@Injectable()
+export class MovieService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAllMovies(): Promise<Movie[]> {
+    return this.prisma.movie.findMany();
+  }
+
+  async getMovieById(id: number): Promise<Movie> {
+    return this.prisma.movie.findFirstOrThrow({
+      where: {
+        id,
+      },
+    });
+  }
+}
